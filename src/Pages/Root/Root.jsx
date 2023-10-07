13f0a1c5fbd7a7dd4b8706daba3c4845551813e1
SakibAlHasan10 <sakibalhasan.me1@gmail.com>
2023-10-07 10:22:28 +0600

    create footer section

--- src/Pages/Root/Root.jsx
+++ src/Pages/Root/Root.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../../Section/Navbar/Navbar";
+import Footer from "../../Section/Footer/Footer";
 
 const Root = () => {
     return (
         <div>
             <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
