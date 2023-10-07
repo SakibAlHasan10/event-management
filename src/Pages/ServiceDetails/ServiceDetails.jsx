3e864429de8a1fa813b05c502c851ba774abc254
SakibAlHasan10 <sakibalhasan.me1@gmail.com>
2023-10-07 18:52:26 +0600

    add service single pages

--- src/Pages/ServiceDetails/ServiceDetails.jsx
+++ src/Pages/ServiceDetails/ServiceDetails.jsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import useApi from "../../ContextApi/useApi";
 
 const ServiceDetails = () => {
@@ -8,11 +8,17 @@ const ServiceDetails = () => {
     const {img, name, description, price} = service
     console.log(id, ourServices, service)
     return (
-        <div className="mt-20">
-            <img src={img} alt="" />
-            <h2>{name}</h2>
-            <p>{description}</p>
-            <p>{price}</p>
+        <div className="pt-36 bg-slate-200 pb-12">
+            <div className="bg-white px-8 py-6  max-w-6xl mx-auto">
+            <h2 className="text-4xl font-semibold">{name}</h2>
+            <img src={img} alt="" className="w-[80%] border mt-8 mb-6" />
+            <p className="text-justify pb-5">{description}</p>
+            <p className="text-black text-2xl font-bold">${price}</p>
+            <Link to={'/'}>
+            <button className="w-40 mt-7 btn bg-red-600 hover:bg-red-400 text-white">Back</button>
+            </Link>
+
+            </div>
         </div>
     );
 };
