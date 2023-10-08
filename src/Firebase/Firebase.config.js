66ad6503d6029bf3aeb0803975477291dc2fc722
SakibAlHasan10 <sakibalhasan.me1@gmail.com>
2023-10-08 08:41:16 +0600

    install firebasee

---
+++ src/Firebase/Firebase.config.js
@@ -0,0 +1,11 @@
+
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyBWeR0wshivhhQH3xqP-59k0W_Dg8PAQqc",
+  authDomain: "our-sports.firebaseapp.com",
+  projectId: "our-sports",
+  storageBucket: "our-sports.appspot.com",
+  messagingSenderId: "171821637020",
+  appId: "1:171821637020:web:652f7341b13e0402f13c59"
+};
+export const app = initializeApp(firebaseConfig);
